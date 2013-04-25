473773331473968a90425e01f7598e915f71c45d
shareLee <shareLee@shareLee-PC>
2013-04-26 01:16:57 +0800

    ad

---
+++ src/com/msgm/action/TestAction.java
@@ -0,0 +1,12 @@
+package com.msgm.action;
+
+public class TestAction {
+	private String wings;
+	private String Lee;
+	private String Lee1;
+	private String Lee2;
+	private String Lee3;
+	private String Lee4;
+	private String Lee5;
+	private String Lee6;
+}
